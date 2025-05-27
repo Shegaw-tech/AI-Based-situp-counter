9fd7d1d68f289b18ad48606f6aeb8c0441a871f7
Gebra Shegaw Yeshidnber <shegyg1221@gmail.com>
2025-05-28 00:27:53 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -91,4 +91,4 @@ def handle_server_error(e):
     }), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
